415a38c2b27724b3c44b38791c8d3d581433e440
tankmkni <62043937+tankmkni@users.noreply.github.com>
2020-10-27 23:41:44 +0900

    add code

--- test.js
+++ test.js
@@ -1,3 +1,10 @@
-function test(){
-	alert(document.getElementById("elem").contentWindow.document.getElementById("target"));
+function test(){	// 親要素のボタン操作で子要素の値を操作
+
+	var iframeElem = document.getElementById("elem");	// iframe要素の取得
+	var innerElem = iframeElem.contentWindow.document;	// iframe内の要素の取得
+	var target = innerElem.getElementById("target");	// target要素の取得
+
+	// iframe内のtarget要素を操作
+	target.innerHTML = "親要素の操作で子要素を変更";
+	rarget.setAttribute("color","skyblue");
 }
